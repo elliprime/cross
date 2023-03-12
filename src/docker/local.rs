95ef25853dd8b3a56d9763b49233e00b31ab02bd
dizzy <7085125-dizzycodes@users.noreply.gitlab.com>
2023-03-11 23:06:10 -0800

    remove another use of keeping the cargo bin out of the docker container

--- src/docker/local.rs
+++ src/docker/local.rs
@@ -77,7 +77,8 @@ pub(crate) fn run(
             ),
         ])
         // Prevent `bin` from being mounted inside the Docker container.
-        .args(["-v", &format!("{}/bin", toolchain_dirs.cargo_mount_path())]);
+        //.args(["-v", &format!("{}/bin", toolchain_dirs.cargo_mount_path())])
+    ;
 
     docker.args([
         "-v",
